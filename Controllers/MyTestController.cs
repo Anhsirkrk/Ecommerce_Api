d03c04ee47f939f7236a1a0fa64dde34f75bfbbe
BHASKAR VVSG <bhaskarvvsg57hds@gmail.com>
2023-10-09 10:23:58 +0530

    Added MyTestController

---
+++ Controllers/MyTestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce_Api.Controllers
+{
+    public class MyTestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
